1e7605b749ebe1d038f8f84d267353c5f13bc3c4
milieu-dev <milieu.dev@gmail.com>
2025-12-11 01:44:38 +0900

    chore: `컨텍스트 프로바이더` 정리

--- src/providers/analytics/index.tsx
+++ src/providers/analytics/index.tsx
@@ -3,14 +3,13 @@ import Analytics from "analytics";
 import type { PropsWithChildren } from "react";
 import { AnalyticsProvider as AP } from "use-analytics";
 
-const analytics = Analytics({
-	plugins: [
-		googleAnalytics({
-			measurementIds: [import.meta.env.VITE_GA_ID],
-		}),
-	],
-});
-
 export default function AnalyticsProvider(props: PropsWithChildren) {
+	const analytics = Analytics({
+		plugins: [
+			googleAnalytics({
+				measurementIds: [process.env.NEXT_PUBLIC_GA_ID || ""],
+			}),
+		],
+	});
 	return <AP instance={analytics}>{props.children}</AP>;
 }
