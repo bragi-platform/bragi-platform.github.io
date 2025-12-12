72a20f8762f390d91cf60faddd8fb6f4266fc193
milieu-dev <milieu.dev@gmail.com>
2025-12-12 14:03:01 +0900

    refactor: 기존의 `a` 태그를 `Link` 컴포넌트로 대체 (#11)

--- src/layout/header/index.tsx
+++ src/layout/header/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Logo from "@/assets/logo.svg";
 import { Button } from "@/components/ui/button";
 import ThemeToggle from "./ThemeToggle";
@@ -7,12 +8,12 @@ export default function Header() {
 		<header className="flex w-full justify-between font-plex-sans">
 			<div className="flex gap-2">
 				<Button variant={"ghost"} className="flex h-fit py-2">
-					<a href="/" className="flex gap-2">
+					<Link href="/" className="flex gap-2">
 						<Logo className="min-w-8 min-h-8" />
 						<span className="flex gap-2 items-center text-lg font-extrabold">
 							브라기
 						</span>
-					</a>
+					</Link>
 				</Button>
 			</div>
 
